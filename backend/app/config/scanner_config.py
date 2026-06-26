2dd8d8dbaa3000294703f621e28b60c765760dd9
dattatrayapawar09 <dattapawar0902@gmail.com>
2026-06-26 16:41:06 +0530

    Create scanner_config.py

---
+++ backend/app/config/scanner_config.py
@@ -0,0 +1,65 @@
+TOP_RESULTS = 50
+
+STRIKE_RANGE = 10
+
+MIN_VOLUME_RATIO = 1.5
+
+INDEX_SYMBOLS = [
+    "NIFTY",
+    "BANKNIFTY",
+    "FINNIFTY",
+    "SENSEX"
+]
+
+TOP_50_STOCKS = [
+    "RELIANCE",
+    "HDFCBANK",
+    "ICICIBANK",
+    "SBIN",
+    "INFY",
+    "TCS",
+    "LT",
+    "BHARTIARTL",
+    "AXISBANK",
+    "KOTAKBANK",
+    "MARUTI",
+    "TITAN",
+    "ASIANPAINT",
+    "ULTRACEMCO",
+    "BAJFINANCE",
+    "BAJAJFINSV",
+    "HCLTECH",
+    "TECHM",
+    "WIPRO",
+    "SUNPHARMA",
+    "TATAMOTORS",
+    "M&M",
+    "ADANIENT",
+    "ADANIPORTS",
+    "NTPC",
+    "POWERGRID",
+    "ONGC",
+    "COALINDIA",
+    "BEL",
+    "HAL",
+    "JSWSTEEL",
+    "TATASTEEL",
+    "HINDALCO",
+    "GRASIM",
+    "CIPLA",
+    "DRREDDY",
+    "EICHERMOT",
+    "HEROMOTOCO",
+    "BAJAJ_AUTO",
+    "INDUSINDBK",
+    "PNB",
+    "CANBK",
+    "IOC",
+    "BPCL",
+    "SHRIRAMFIN",
+    "TRENT",
+    "DLF",
+    "LODHA",
+    "DIVISLAB",
+    "PIDILITIND"
+]
