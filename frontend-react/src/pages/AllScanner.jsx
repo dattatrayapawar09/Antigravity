b2c97b749bf03f797bbc55b0c527d199faa47301
dattatrayapawar09 <dattapawar0902@gmail.com>
2026-06-27 20:16:21 +0530

    Create AllScanner.jsx

---
+++ frontend-react/src/pages/AllScanner.jsx
@@ -0,0 +1,9 @@
+export default function AllScanner() {
+  return (
+    <div className="p-6 text-white">
+      <h1 className="text-3xl font-bold">
+        🌐 All Scanner
+      </h1>
+    </div>
+  );
+}
