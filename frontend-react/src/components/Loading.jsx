414c0f4fa0c2371ec09a377f1846ebfb44b41ec7
dattatrayapawar09 <dattapawar0902@gmail.com>
2026-06-27 20:42:49 +0530

    Create Loading.jsx

---
+++ frontend-react/src/components/Loading.jsx
@@ -0,0 +1,13 @@
+export default function Loading() {
+
+  return (
+
+    <div className="flex h-64 items-center justify-center">
+
+      <div className="h-12 w-12 animate-spin rounded-full border-4 border-cyan-500 border-t-transparent"></div>
+
+    </div>
+
+  );
+
+}
