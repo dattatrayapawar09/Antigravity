bae266acf2bc185485a4780d2331e6c91fe57ec4
dattatrayapawar09 <dattapawar0902@gmail.com>
2026-06-27 20:35:04 +0530

    Update StockOptions.jsx

--- frontend-react/src/pages/StockOptions.jsx
+++ frontend-react/src/pages/StockOptions.jsx
@@ -1,9 +1,65 @@
+import { useEffect, useState } from "react";
+
+import { useScanner } from "../context/ScannerContext";
+
+import MarketCards from "../components/MarketCards";
+import Filters from "../components/Filters";
+import ScannerTable from "../components/ScannerTable";
+
 export default function StockOptions() {
+  const {
+    options,
+    expiries,
+    refreshScanner,
+    setActiveTab,
+  } = useScanner();
+
+  const [filteredOptions, setFilteredOptions] = useState([]);
+
+  useEffect(() => {
+    setActiveTab("stocks");
+  }, [setActiveTab]);
+
+  useEffect(() => {
+    setFilteredOptions(options);
+  }, [options]);
+
   return (
-    <div className="p-6 text-white">
-      <h1 className="text-3xl font-bold">
-        🏢 Stock Options
-      </h1>
+    <div className="space-y-6">
+
+      {/* Page Header */}
+
+      <div>
+
+        <h1 className="text-3xl font-bold">
+          🏢 Stock Options
+        </h1>
+
+        <p className="mt-1 text-slate-400">
+          Top 50 F&O Stocks • Monthly Expiry • Ranked by Smart Score
+        </p>
+
+      </div>
+
+      {/* Market Cards */}
+
+      <MarketCards />
+
+      {/* Filters */}
+
+      <Filters
+        options={options}
+        expiries={expiries}
+        onFilterChange={setFilteredOptions}
+        onRefresh={refreshScanner}
+      />
+
+      {/* Scanner Table */}
+
+      <ScannerTable
+        data={filteredOptions}
+      />
+
     </div>
   );
 }
