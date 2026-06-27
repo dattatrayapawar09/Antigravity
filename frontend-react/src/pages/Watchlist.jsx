9cee5389e8393a47b4cd23dbf96d2a681256e886
dattatrayapawar09 <dattapawar0902@gmail.com>
2026-06-27 20:16:48 +0530

    Create Watchlist.jsx

---
+++ frontend-react/src/pages/Watchlist.jsx
@@ -0,0 +1,9 @@
+export default function Watchlist() {
+  return (
+    <div className="p-6 text-white">
+      <h1 className="text-3xl font-bold">
+        ⭐ Watchlist
+      </h1>
+    </div>
+  );
+}
