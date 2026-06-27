62f003ad18b5cfebe4a1d65fbd92000fb8d1cd31
dattatrayapawar09 <dattapawar0902@gmail.com>
2026-06-27 20:16:00 +0530

    Create StockOptions.jsx

---
+++ frontend-react/src/pages/StockOptions.jsx
@@ -0,0 +1,9 @@
+export default function StockOptions() {
+  return (
+    <div className="p-6 text-white">
+      <h1 className="text-3xl font-bold">
+        🏢 Stock Options
+      </h1>
+    </div>
+  );
+}
